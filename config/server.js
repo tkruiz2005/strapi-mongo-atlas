1f6a2cb770b261be107cdcbbd4aed042b0d8ad7e
tkruiz2005 <tkruiz2005@gmail.com>
2021-02-24 22:13:43 -0300

    make it better

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8c2c209fb181b1204e9a5f53ce4cef8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a082a316-c84c-42d4-bcf2-12c84e3fd5b2'
+};
