ee8c7097b3c8c0af1efe65e065f98a3284db0ceb
root <tkruiz2005@gmail.com>
2021-02-25 12:50:55 -0300

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -3,10 +3,18 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'mongoose',
-      settings: {},
-      options: {},
+      settings: {
+        host: env('DATABASE_HOST', 'my-api-strapi-db.afrkz.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'my-api-strapi-db'),
+        username: env('DATABASE_USERNAME', 'tkruiz2005'),
+        password: env('DATABASE_PASSWORD', 'elvira10'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
 });
-
-
