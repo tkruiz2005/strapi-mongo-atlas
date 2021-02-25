61c8d6e08681546fdd9b960041b3ea9b2320b430
root <tkruiz2005@gmail.com>
2021-02-25 13:03:59 -0300

    Initial Commit1

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,13 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        host: env('DATABASE_HOST', 'my-api-strapi-db.afrkz.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'my-api-strapi-db'),
+        username: env('DATABASE_USERNAME', 'tkruiz2005'),
+        password: env('DATABASE_PASSWORD', 'elvira10'),
       },
-      options: {},
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,12 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST', 'my-api-strapi-db.afrkz.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'my-api-strapi-db'),
+        username: env('DATABASE_USERNAME', 'tkruiz2005'),
+        password: env('DATABASE_PASSWORD', 'elvira10'),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
 });
-
