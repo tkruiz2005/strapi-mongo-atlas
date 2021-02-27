29d0ca16d751d3a242e2d04083a2410035174ceb
root <tkruiz2005@gmail.com>
2021-02-26 23:36:59 -0300

    change port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 80),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'c8c2c209fb181b1204e9a5f53ce4cef8'),
